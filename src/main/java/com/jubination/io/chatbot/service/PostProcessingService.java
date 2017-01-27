abc8bc42fc978676c44181165cd0f2d3be0b2521
souvik131 <souvik131@gmail.com>
2017-01-27 18:37:34 +0530

    in progress

--- src/main/java/com/jubination/io/chatbot/service/PostProcessingService.java
+++ src/main/java/com/jubination/io/chatbot/service/PostProcessingService.java
@@ -8,7 +8,6 @@ package com.jubination.io.chatbot.service;
 
 import com.jubination.io.chatbot.backend.pojo.core.ChatBotRequest;
 import com.jubination.io.chatbot.backend.service.core.DashBotUpdater;
-import com.jubination.io.chatbot.backend.service.core.RandomNumberGenerator;
 import com.jubination.io.chatbot.model.dao.DashBotDAO;
 import com.jubination.io.chatbot.model.dao.UserDAO;
 import com.jubination.io.chatbot.model.pojo.Chatlet;
@@ -37,13 +36,11 @@ public class PostProcessingService {
     DashBotDAO dashBotRepository;
           @Autowired
           DashBotUpdater dashBotUpdater;
-          @Autowired
-          RandomNumberGenerator gen;
       
-      private static final int imageQuoteCount=17;
-      private static final int imageAgeCount=3;
-      private static final int imageHabitsCount=5;
-      private static final int imageStressCount=4;
+      private static final int imageQuoteCount=16;
+      private static final int imageAgeCount=2;
+      private static final int imageHabitsCount=4;
+      private static final int imageStressCount=3;
       private static final int lineBreak=45;
       private static final String link = "mailto:?subject=I wanted you to see this&amp;body=Check out Mia http://bit.ly/2jYG0xz";
       
@@ -579,16 +576,16 @@ public class PostProcessingService {
                 }
             }
             if(tag.contains("age")){
-                path+="-"+gen.generate(imageAgeCount);
+                path+="-"+new Random().nextInt(imageAgeCount);
             }
             else if(tag.contains("habits")){
-                path+="-"+gen.generate(imageHabitsCount);
+                path+="-"+new Random().nextInt(imageHabitsCount);
             }
             else if(tag.contains("quote")){
-                path+="-"+gen.generate(imageQuoteCount);
+                path+="-"+new Random().nextInt(imageQuoteCount);
             }
              else if(tag.contains("stress")){
-                path+="-"+gen.generate(imageStressCount);
+                path+="-"+new Random().nextInt(imageStressCount);
             }
             return preTag+path+postTag;
         }
