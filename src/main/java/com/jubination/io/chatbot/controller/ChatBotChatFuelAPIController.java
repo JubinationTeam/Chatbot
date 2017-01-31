b675b13243586138a205fdd8876fed5fe9760b15
souvik131 <souvik131@gmail.com>
2017-01-31 17:57:21 +0530

    in progress

--- src/main/java/com/jubination/io/chatbot/controller/ChatBotChatFuelAPIController.java
+++ src/main/java/com/jubination/io/chatbot/controller/ChatBotChatFuelAPIController.java
@@ -97,7 +97,7 @@ public class ChatBotChatFuelAPIController {
            
            ChatFuelet fuelet=new ChatFuelet();
            Message message=new Message("initialized");
-           message.getQuick_replies().add(new QuickReplies("init"));
+           message.getQuick_replies().add(new QuickReplies(""));
            fuelet.getMessages().add(message);
              
             return fuelet;
